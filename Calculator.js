5952ea3fdf286f980f743dc5fa244ba77ee053ed
Karen <mr.karen.hunanyan@gmail.com>
2026-02-22 13:06:14 +0400

    First program

---
+++ Calculator.js
@@ -0,0 +1,30 @@
+//Calculator program
+
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+let operator = prompt("Enter the operator (+, -, *, /):");
+
+let result;
+
+switch (operator) {
+    case "+":
+        result = num1 + num2;
+        break;
+    case "-":
+        result = num1 - num2;
+        break;
+    case "*":
+        result = num1 * num2;
+        break;
+    case "/":
+        if (num2 !== 0) {
+            result = num1 / num2;
+        } else {
+            result = "Error: Division by zero is not allowed.";
+        }
+        break;
+    default:
+        result = "Error: Invalid operator.";
+}
+
+alert("The result is: " + result);
